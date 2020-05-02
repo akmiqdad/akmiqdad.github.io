a8fb89f6c7b85da90ceb5cfbc8102c0f12adda2f
Muhammed-Miqdad <muhammedmiqdadtkd@gmail.com>
2020-05-02 18:13:47 +0530

    test

---
+++ index.js
@@ -0,0 +1,16 @@
+// Load the Express package as a module
+const express = require("express");
+
+// Access the exported service
+const app = express();
+
+// Return a string for requests to the root URL ("/")
+app.get("/", (request, response) => {
+  response.send("Hello from Express!");
+});
+
+// Start listening to incoming requests
+// If process.env.PORT is not defined, port number 3000 is used
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`);
+});
